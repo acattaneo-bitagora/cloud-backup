d9d868f398d6e7a70c903001c6c2d1abb036c79d
Andrea Cattaneo <a.cattaneo@bitagora.it>
2024-09-09 09:22:40 +0200

    init

---
+++ config.sh
@@ -0,0 +1,16 @@
+BUCKET_ENDPOINT_URL="https://s3.bitagoraorobica.cubbit.eu"
+BUCKET_NAME="bitagoraorobica-bucket-prova01"
+SUCCESS_FILE="/var/cloud-backup/success"
+# FOLDER_TO_PROCESS="/var/lib/vz/dump/"
+FOLDER_TO_PROCESS="/var/prova"
+KEYFILE="/var/cloud-backup/cloud.key"
+
+LOG_FILE="/var/cloud-backup/cloud-backup.log"
+
+
+export BUCKET_ENDPOINT_URL
+export BUCKET_NAME
+export SUCCESS_FILE
+export FOLDER_TO_PROCESS
+export KEYFILE
+export LOG_FILE
---
+++ restore.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Questo script decifra e ripristina tutti i file in una cartella da un bucket S3
+# I file vengono decifrati con GPG e scaricati da S3 con l'utility AWS CLI
+# I file vengono scaricati con la stessa struttura di cartelle della cartella remota
+# I file vengono scaricati in una cartella passata come primo parametro allo script
+
+# Requisiti:
+# - AWS CLI
+# - GPG
+# - Il file chiave per la cifratura
+# - Un bucket S3 con le credenziali configurate in AWS CLI
+# - Un file di configurazione config.sh con le variabili BUCKET_ENDPOINT_URL, BUCKET_NAME, KEYFILE
+
+set -e
+
+# lettura configurazione
+pushd "$(dirname "${BASH_SOURCE[0]}")"
+source ./config.sh
+popd
+
+# Semplice funzione per loggare un messaggio
+# usage: log <message>
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a "$LOG_FILE"
+}
+
+# Funzione per scaricare un singolo file da S3
+# usage: download <filename>
+#   filename: nome del file remoto
+download() {
+    local filename="$1"
+    local remote_filename="$BUCKET_NAME/${filename#/}"
+    log "Scaricamento di s3://$remote_filename ..."
+    if aws s3 --endpoint-url "$BUCKET_ENDPOINT_URL" cp "s3://$remote_filename" -; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Funzione per decifrare e scaricare un singolo file
+# usage: decrypt_and_download <remote_filename> <local_filename>
+#   remote_filename: percorso del file remoto
+#   local_filename: percorso del file locale
+decrypt_and_download() {
+    local remote_filename="$1"
+    local local_filename="$2"
+    local local_folder=$(dirname "$local_filename")
+    mkdir -p "$local_folder"
+    log "Elaborazione di $remote_filename > $local_filename ..."
+    
+    if [ ! -f "$KEYFILE" ]; then
+        log "File chiave non trovato: $KEYFILE"
+        return 1
+    fi
+    if download "${remote_filename}" | gpg --batch --yes --passphrase-file "$KEYFILE" --decrypt --output "$local_filename"; then
+        log "File $remote_filename decifrato con file chiave e scaricato con successo"
+    else
+        log "Errore durante la decifratura o il scaricamento di $remote_filename"
+        download "${remote_filename}" | tee "$local_filename"
+        return 1
+    fi
+}
+
+# main 
+DESTINATION_FOLDER="$@"
+
+# check parameters
+if [ -z "$DESTINATION_FOLDER" ]; then
+    log "Specificare la cartella di destinazione come primo parametro"
+    exit 1
+fi
+
+mkdir -p "$DESTINATION_FOLDER"
+
+# Scarica e decifra tutti i file
+aws s3 --endpoint-url "$BUCKET_ENDPOINT_URL" ls  --recursive "s3://$BUCKET_NAME" | while read -r line; do
+    filename=$(echo "$line" | awk '{print $4}')
+    if [ -z "$filename" ]; then
+        continue
+    fi
+    destination_file="${DESTINATION_FOLDER%/}/${filename#/}"
+    decrypt_and_download "$filename" "${destination_file%\.gpg}"
+done
+
